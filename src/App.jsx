5dacb744ceee8ac734c27a14f63ccc50e93d3b70
Yurii Timofeyev <georgius@ural.ru>
2019-11-09 14:12:28 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+function App() {
+  return (
+    <div className="text-center font-sans">
+      <header className="bg-gray-600 m-6 p-6 rounded shadow-lg">
+        <h1 className="text-white text-3xl">This is a React app</h1>
+      </header>
+      <p className="text-base">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+    </div>
+  )
+}
+export default App
